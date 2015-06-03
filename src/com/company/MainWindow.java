41da57da4144630307971e121e0c44fbacc5f436
bumb0ks <bum.boks@hotmail.com>
2015-06-03 06:16:57 +0300

    Kirill commiting

--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-
+//Kiril Commentit kod
 public class MainWindow extends JFrame {
 
     private JList mList;
